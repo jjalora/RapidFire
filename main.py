031dbf0746a8bcb8f2ae6917ee84bc7eb5d21701
jjalora <jjalora@stanford.edu>
2023-10-07 15:59:06 -0700

    Added functionalities from Chris and Elisabeth

--- main.py
+++ main.py
@@ -74,98 +74,166 @@ page = st.sidebar.selectbox(
 )
 
 if page == "Module 1: Mad Libs":
-    # Initialize the progress bar
-    progress_bar = st.progress(0)
+    # Initial state variable to check if user has started after filling required fields
+    if "has_started" not in st.session_state:
+        st.session_state.has_started = False
 
-    # Initialize state variables
-    if "ratings" not in st.session_state:
-        st.session_state.ratings = []
     if "essay_count" not in st.session_state:
         st.session_state.essay_count = 0
-    if "context" not in st.session_state:
-        st.session_state.context = ""
-    if "all_strong_essays" not in st.session_state:
-        st.session_state.all_strong_essays = []
+
     if "generate" not in st.session_state:
         st.session_state.generate = False
 
-    # Update the progress bar based on the number of essays rated
-    if st.session_state.essay_count:
-        progress_percentage = st.session_state.essay_count / SETTINGS['num_rounds']
-        progress_bar.progress(progress_percentage)
+    if "all_strong_essays" not in st.session_state:
+        st.session_state.all_strong_essays = []
+    
+    if "ratings" not in st.session_state:
+        st.session_state.ratings = []
 
-    # Dropdown for common_prompt
-    selected_common_prompt = st.selectbox("Select a Common Prompt:", common_prompt)
 
-    # Display identity and wildcard options in columns
-    col1, col2 = st.columns(2)
+    if not st.session_state.has_started:
+        st.header("RapidFire: Crafting Authentic College Essays with AI & Human Mentorship")
+        st.write("(*) Please fill in the required information:")
 
-    # Using radio buttons for identity on the left column
-    selected_identity = col1.radio("Choose an Identity:", identity)
+        # Organize inputs across columns for better visuals
+        col1, col2 = st.columns(2)
 
-    # Using radio buttons for wildcard on the right column
-    selected_wildcard = col2.radio("Choose a Wildcard:", wildcard)
+        with col1:
+            st.session_state.first_name = st.text_input("First Name*")
+            st.session_state.last_name = st.text_input("Last Name*")
+            st.session_state.email = st.text_input("Email*")
+            st.session_state.zip_code = st.text_input("Zip Code*")  # New Zip Code field
+            st.session_state.grade = st.text_input("Grade*")
 
-    # Generate button
-    if st.button("Generate"):
-        st.session_state.generate = True
+        with col2:
+            st.session_state.school = st.text_input("School*")  # Moved to the right column
+            st.session_state.counselor_name = st.text_input("Counselor’s Name*")
+            st.session_state.counselor_email = st.text_input("Counselor Email*")
+            st.session_state.top_schools = st.text_area("Top three schools you are applying to (comma separated)")
 
-        prompt_with_attributes = module_one_prompt.format(
-            strong_attribute=strong_attr,
-            weak_attribute=weak_attr,
-            identity=selected_identity,
-            wildcard=selected_wildcard,
-            common_essay_prompt=selected_common_prompt
-        )
+        # Check if all required fields are filled
+        if st.session_state.first_name and st.session_state.last_name and st.session_state.email and \
+           st.session_state.school and st.session_state.grade and st.session_state.counselor_name and \
+           st.session_state.counselor_email and st.session_state.zip_code:
+               
+            # To pseudo-center the Start button
+            start_col1, start_btn_col, start_col2 = st.columns([1, 2, 1])
 
-        module_one_output = get_module_one_output(prompt_with_attributes)
+            if start_btn_col.button("Start the Process of Crafting Your Authentic College Essay"):
+                st.session_state.has_started = True
+                st.experimental_rerun()
 
-        # Parsing the output to get strong and weak essays
-        start_strong = module_one_output.find('Strong Essay') + len('Strong Essay') + 1
-        start_weak = module_one_output.find('Weak Essay')
-        end_strong = start_weak - 1
+    else:
+        if st.session_state.essay_count >= SETTINGS['num_rounds']:
+            st.session_state.module_completed = True
+            st.header("Module 1 Completion!")
+            st.progress(1.0)  # Show progress bar at 100%
+            st.write(f"You've successfully rated {SETTINGS['num_rounds']} essays. Please proceed to Module 2 in the sidebar.")
+        else:
+            st.info(f"""
+                    This module provides examples of both strong and weak essays written by a fictional student. 
+                    To advance to the next module, you must review and rate the stronger essay in {SETTINGS['num_rounds']} separate comparisons. 
+                    Please note that generating the essays may take some time. Be patient.
+                    """)
+            # Initialize the progress bar
+            progress_bar = st.progress(0)
 
-        st.session_state.strong_essay = module_one_output[start_strong:end_strong].strip()
-        st.session_state.weak_essay = module_one_output[start_weak + len('Weak Essay') + 1:].strip()
+            # Update the progress bar based on the number of essays rated
+            if st.session_state.essay_count:
+                progress_percentage = st.session_state.essay_count / SETTINGS['num_rounds']
+                progress_bar.progress(progress_percentage)
 
-        # Append the new strong essay to the list
-        st.session_state.all_strong_essays.append(st.session_state.strong_essay)
+            # Dropdown for common_prompt
+            selected_common_prompt = st.selectbox("Select a Common Prompt:", common_prompt)
 
-    # If essays are generated, display them
-    if st.session_state.generate:
-        col_strong, col_weak = st.columns(2)
+            # Display identity and wildcard options in columns
+            col1, col2 = st.columns(2)
 
-        with col_strong:
-            st.subheader("Strong Essay")
-            st.text_area("", value=st.session_state.strong_essay, height=400)
+            # Using radio buttons for identity on the left column
+            selected_identity = col1.radio("Choose an Identity:", identity)
 
-        with col_weak:
-            st.subheader("Weak Essay")
-            st.text_area("", value=st.session_state.weak_essay, height=400)
+            # Using radio buttons for wildcard on the right column
+            selected_wildcard = col2.radio("Choose a Wildcard:", wildcard)
 
-        with col_strong:
-            st.subheader("Rate the Strong Essay:")
+            # Create columns for the Generate and Shuffle buttons
+            btn_generate_col, btn_shuffle_col = st.columns([1, 1])
 
-            # Creating a row of buttons
-            cols = st.columns(5)
-            for i in range(1, 6):
-                if cols[i-1].button(str(i), key=f"rate_{i}"):
-                    st.session_state.ratings.append(i)
-                    st.session_state.essay_count += 1
-                    
-                    # Refresh the identity and wildcard options
-                    st.session_state.identity = random.sample(load_file_to_list(join(pathToPrompts, "identity.txt")), 4)
-                    st.session_state.wildcard = random.sample(load_file_to_list(join(pathToPrompts, "wildcard.txt")), 4)
-                    
-                    st.session_state.generate = False  # Reset generation state after rating
+            # Generate button in the left (btn_generate_col) column
+            if btn_generate_col.button("Generate"):
+                st.session_state.generate = True
 
-                    # Refresh the page to show new selections and hide the essays
-                    st.experimental_rerun()
+                prompt_with_attributes = module_one_prompt.format(
+                    strong_attribute=strong_attr,
+                    weak_attribute=weak_attr,
+                    identity=selected_identity,
+                    wildcard=selected_wildcard,
+                    common_essay_prompt=selected_common_prompt
+                )
 
-        if st.session_state.ratings:
-            st.write(f"You rated the last essay {st.session_state.ratings[-1]} out of 5.")
+                module_one_output = get_module_one_output(prompt_with_attributes)
+
+                # Parsing the output to get strong and weak essays
+                start_strong = module_one_output.find('Strong Essay') + len('Strong Essay') + 1
+                start_weak = module_one_output.find('Weak Essay')
+                end_strong = start_weak - 1
+
+                st.session_state.strong_essay = module_one_output[start_strong:end_strong].strip()
+                st.session_state.weak_essay = module_one_output[start_weak + len('Weak Essay') + 1:].strip()
+
+                # Append the new strong essay to the list
+                st.session_state.all_strong_essays.append(st.session_state.strong_essay)
+            
+            # Shuffle button in the right (btn_shuffle_col) column
+            if btn_shuffle_col.button('Shuffle Options'):
+                st.session_state.identity = random.sample(load_file_to_list(join(pathToPrompts, "identity.txt")), 4)
+                st.session_state.wildcard = random.sample(load_file_to_list(join(pathToPrompts, "wildcard.txt")), 4)
+                st.experimental_rerun()
+
+            # If essays are generated, display them
+            if st.session_state.generate:
+                col_strong, col_weak = st.columns(2)
+
+                with col_strong:
+                    st.subheader("Strong Essay")
+                    st.text_area("", value=st.session_state.strong_essay, height=400)
+
+                with col_weak:
+                    st.subheader("Weak Essay")
+                    st.text_area("", value=st.session_state.weak_essay, height=400)
+
+                with col_strong:
+                    st.subheader("Rate the Strong Essay:")
+
+                    # Creating a row of buttons
+                    cols = st.columns(5)
+                    rerun_flag = False
+                    for i in range(1, 6):
+                        if cols[i-1].button(str(i), key=f"rate_{i}"):
+                            st.session_state.ratings.append(i)
+                            st.session_state.essay_count += 1
+
+                            # Refresh the identity and wildcard options
+                            st.session_state.identity = random.sample(load_file_to_list(join(pathToPrompts, "identity.txt")), 4)
+                            st.session_state.wildcard = random.sample(load_file_to_list(join(pathToPrompts, "wildcard.txt")), 4)
+
+                            st.session_state.generate = False  # Reset generation state after rating
+                            rerun_flag = True
+
+                    if rerun_flag:
+                        if st.session_state.essay_count >= SETTINGS['num_rounds']:
+                            st.session_state.module_completed = True
+                            st.experimental_rerun()
+                        else:
+                            st.experimental_rerun()
+
+                if st.session_state.ratings:
+                    st.write(f"You rated the last essay {st.session_state.ratings[-1]} out of 5.")
+
 elif page == "Module 2: Brainstorm":
     st.header("Module 2: Brainstorm")
+
+    if "context" not in st.session_state:
+        st.session_state.context = ""
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
